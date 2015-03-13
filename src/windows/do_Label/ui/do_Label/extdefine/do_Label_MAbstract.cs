160eafc84691fe48cf988a5b85fd856b04af5f21
zhouyi <643988878@qq.com>
2015-03-13 17:01:14 +0800

    label

---
+++ src/windows/do_Label/ui/do_Label/extdefine/do_Label_MAbstract.cs
@@ -0,0 +1,26 @@
+﻿using doCore.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_Label.extdefine
+{
+    public abstract class do_Label_MAbstract : doUIModule
+    {
+        protected do_Label_MAbstract()
+            : base()
+        {
+            
+        }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public override void OnInit()
+        {
+            base.OnInit();
+            //注册属性
+        }
+    }
+}
