1cb36aa217eb4048dbe981faf1f44811daa75c8e
zhouyi <643988878@qq.com>
2015-03-18 11:28:28 +0800

    label 18

--- src/windows/do_Label/ui/do_Label/Properties/AssemblyInfo.cs
+++ src/windows/do_Label/ui/do_Label/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("doExt_do_Label")]
+[assembly: AssemblyTitle("do_Label")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("doExt_do_Label")]
+[assembly: AssemblyProduct("do_Label")]
 [assembly: AssemblyCopyright("版权所有(C)  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
